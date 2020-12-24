0b487631c08378d3cb1e0043d5d6bb4204e8def4
Huang Jian-Min <yfr.huang@hotmail.com>
2020-12-24 12:11:37 +0800

    update

--- sections/Section 5/demo-jpa/src/main/java/com/example/demo/data/enu/TestEnum.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.data.enu;
-
-public enum TestEnum {
-
-    VALUE1, VALUE2, VALUE3
-}
--- sections/Section 5/demo-jpa/src/main/java/com/example/demo/data/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.data.repository;
-
-import com.example.demo.data.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long>, JpaSpecificationExecutor<TestEntity> {
-}
