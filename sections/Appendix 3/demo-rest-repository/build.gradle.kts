3a5d15b63dd50a1eec6a7402e8e9669bbe175440
Huang Jian-Min <yfr.huang@hotmail.com>
2021-04-11 17:32:04 +0800

    update appendix 3

--- sections/Appendix 3/demo-rest-repository/build.gradle.kts
+++ sections/Appendix 3/demo-rest-repository/build.gradle.kts
@@ -19,11 +19,9 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-//	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//	implementation("org.springframework.data:spring-data-rest-hal-explorer")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
