4c345e4f672462413b7bee15e8867fd2d3028b20
Huang Jian-Min <yfr.huang@hotmail.com>
2021-09-09 00:14:04 +0800

    update project

---
+++ sections/day10/demo-schedule-type1/src/test/kotlin/com/example/demo/schedule/type1/DemoScheduleType1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo.schedule.type1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoScheduleType1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
