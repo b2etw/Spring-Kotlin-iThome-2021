95330af64ffc7e6ab6e4b562fe8598fa915e3c47
Jian-Min Huang <vincent.huang@funpodium.net>
2021-09-26 19:22:30 +0800

    only day 25

---
+++ sections/day25/bdd/src/test/kotlin/cc/jianminhuang/day25/BddApplicationTests.kt
@@ -0,0 +1,13 @@
+package cc.jianminhuang.day25
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BddApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
